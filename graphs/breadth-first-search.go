6cdd0839475f835997ef09244205c79f906e33bc
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-08-27 18:06:43 +0100

    Feat/breadth first search (#3)
    * add: breadth-first-search

---
+++ graphs/breadth-first-search.go
@@ -0,0 +1,21 @@
+package graphs
+
+// Node : graph node
+type Node struct {
+	Name string
+	Children []*Node
+}
+
+// BreadthFirstSearch : time -> O(v + e), space O(v)
+func (n *Node )BreadthFirstSearch(array []string) []string {
+	q := []*Node{n}
+	for len(q) > 0 {
+		current := q[0]
+		q = q[1:]
+		array = append(array, q.Name)
+		for _, child := range q.Children {
+			q = append(q, child)
+		}
+	}
+	return array
+}
