b4e3afcb47b73d60ae1c4352e62f9c3bfb144bba
alexjperkins <alexjperkins@hotmail.co.uk>
2020-08-27 16:09:25 +0000

    add: single cycle check

---
+++ graphs/single_cycle_check.go
@@ -0,0 +1,26 @@
+package graphs
+
+// SingleCycleCheck Complexity:
+// Time: O(n)
+// Space: O(1)
+func SingleCycleCheck(array []int) bool {
+	currentIndex, itemsVisited := 0, 0
+
+	for itemsVisited < len(array) - 1 {
+		if itemsVisited > 0 && itemsVisited < len(array) && currentIndex == 0 {
+			return false
+		}
+
+		if itemsVisited == len(array) {
+			return currentIndex == 0
+		}
+
+		itemsVisited ++
+		nextIndex := (array[current] + current) % len(array)
+		currentIndex = nextIndex
+		if currentIndex < 0 {
+			currentIndex += len(array)
+		}
+	}
+	return false
+}
