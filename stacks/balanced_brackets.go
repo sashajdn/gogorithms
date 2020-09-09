551bfb8dabf967eab8e023e4e59a0427409d22db
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-09-09 21:00:10 +0100

    Feat/balanced brackets (#10)
    * add: balanced brackets problem

---
+++ stacks/balanced_brackets.go
@@ -0,0 +1,80 @@
+package stacks
+
+// Stack : struct
+type Stack struct {
+	stack []interface{}
+}
+
+// NewStack : Factory method
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+// Pop : pops the last value off the stack
+func (s *Stack) Pop() interface{} {
+	v := s.Peek()
+	s.stack = s.stack[:len(s.stack) - 1]
+	return v
+}
+
+// Peek : peeks at the last value in the stack
+func (s *Stack) Peek() interface{} {
+	return s.stack[len(s.stack) - 1]
+}
+
+// Push : pushes a new ``value`` onto the stack
+func (s *Stack) Push(values ...interface{}) {
+	s.stack = append(s.stack, values...)
+}
+
+// BalancedBrackets : checks if string contains balanced brackets
+// Complexity: T -> O(n), S -> O(n)
+func BalancedBrackets(s string) bool {
+	stack := NewStack()
+	bh := newBracketHandler()
+
+	for _, char := range s{
+		if bh.isOpenBracket(char) {
+			stack.Push(char)
+		}
+
+		if bh.isClosedBracket(char) {
+			expected := stack.Pop()
+			if expected != bh.closed2Open[char] {
+				return false
+			}	
+		}
+	}
+	return true
+}
+
+type bracketHandler struct {
+	closed2Open map[rune]rune
+}
+
+func newBracketHandler() *bracketHandler{
+	closed2Open := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	return &bracketHandler{closed2Open}
+}
+
+func  (b *bracketHandler) isOpenBracket(r rune) bool{
+	for _, c := range b.closed2Open {
+		if c == r {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *bracketHandler) isClosedBracket(r rune) bool {
+	for o := range b.closed2Open {
+		if o == r {
+			return true
+		}
+	}
+	return false
+}
---
+++ stacks/balanced_brackets_test.go
@@ -0,0 +1,27 @@
+package stacks
+
+import (
+	"testing"
+)
+
+func TestBalancedBrackets(t *testing.T) {
+	s := "((("
+	expected := false
+	res := BalancedBrackets(s)
+	if res != expected {
+		t.Fatalf("expected: %v, got %v", expected, res)
+	}
+
+	s = "[[({})]]{}([])"
+	expected = true
+	res = BalancedBrackets(s)
+	if res != expected {
+		t.Fatalf("expected: %v, got %v", expected, res)
+	}
+
+	s = "hello (there)! How {do[we(do)]}"
+	res = BalancedBrackets(s)
+	if res != expected {
+		t.Fatalf("expected: %v, got %v", expected, res)
+	}
+}
