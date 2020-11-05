27f658f411550f5263876dd4f0c472ab2c63cc76
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-11-05 15:14:02 +0000

    add: find closest value in a bst (#11)

---
+++ bst/closest_value_in_bst.go
@@ -0,0 +1,53 @@
+package bst
+
+type BST struct {
+	value int
+
+	left  *BST
+	right *BST
+}
+
+func New(value int, left, right *BST) *BST {
+	bst := BST{value, left, right}
+	return &bst
+}
+
+// FindClosestValue T -> O(log(n)), S -> O(1)
+func (tree *BST) FindClosestvalue(target int) int {
+	if tree.value == target {
+		return target
+	}
+
+	diff := abs(tree.value - target)
+
+	if tree.left != nil {
+		if target < tree.value {
+			ldiff := abs(tree.left.value - target)
+
+			if diff < ldiff {
+				return tree.value
+			}
+
+			return tree.left.FindClosestvalue(target)
+		}
+	}
+
+	if tree.right != nil {
+		rdiff := abs(tree.right.value - target)
+
+		if diff < rdiff {
+			return tree.value
+		}
+
+		return tree.right.FindClosestvalue(target)
+	}
+
+	return tree.value
+}
+
+func abs(value int) int {
+	if value > 0 {
+		return value
+	}
+	return value * -1
+}
