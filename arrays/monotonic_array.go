5cd0f578870155563646b377e80d72dc32785070
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2020-11-09 21:53:39 +0000

    Feat/monotonic array (#17)
    * add: monotonic array
    
    * fixup: monotonic array
    
    * fixup: allow pseudo monotonic array

---
+++ arrays/monotonic_array.go
@@ -0,0 +1,32 @@
+package arrays
+
+// IsMonotonicArray T -> O(n), S -> O(1)
+func IsMonotonicArray(array []int) bool {
+	if len(array) < 2 {
+		return true
+	}
+
+	increasing := false
+
+	if array[0] == array[1] {
+		return IsMonotonicArray(array[1:])
+	}
+
+	if array[0] < array[1] {
+		increasing = true
+	}
+
+	for i := 1; i < len(array)-1; i++ {
+		if increasing {
+			if array[i] > array[i+1] {
+				return false
+			}
+		} else {
+			if array[i] < array[i+1] {
+				return false
+			}
+		}
+
+	}
+	return true
+}
