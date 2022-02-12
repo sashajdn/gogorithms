c00d0c7707a12f5f36db06a1328b40123daaa4c5
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-02-12 22:43:52 +0000

    add(graph,binarytree): add graph traversal problems (#66)

--- graphs/depth_first_search.go
+++ graphs/depth_first_search.go
@@ -1,10 +1,31 @@
 package graphs
 
-// DepthFirstSearch
-func (g *Graph) DepthFirstSearch(a []string) []string {
-	a = append(a, g.id)
-	for _, child := range g.children {
-		a = child.DepthFirstSearch(a)
+// depthFirstSearch ...
+//
+// T -> O(v + e) where v is the number of vertices and is the number of edges.
+// S -> O(v)
+func depthFirstSearch(node *GraphNode) []*GraphNode {
+	var output []*GraphNode
+	dfsHelper(node, &output)
+	return output
+}
+
+func dfsHelper(node *GraphNode, collection *[]*GraphNode) {
+	*collection = append(*collection, node)
+	for _, child := range node.Children {
+		dfsHelper(child, collection)
 	}
-	return a
+}
+
+// DepthFirstSearch ...
+//
+// T -> O(v + e) where v is the number of vertices and is the number of edges.
+// S -> O(v)
+func (g *GraphNode) DepthFirstSearch() []*GraphNode {
+	var output = []*GraphNode{g}
+	for _, child := range g.Children {
+		output = append(output, child.DepthFirstSearch()...)
+	}
+
+	return output
 }
