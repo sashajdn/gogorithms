6d12feb7ccce9e0644365664d0400000ce4cde34
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-02-19 21:21:14 +0000

    Feat/two sum (#85)
    * add(arrays): add new two sum case
    
    * add(arrays): two sum iterative

--- arrays/two_num_sum.go
+++ arrays/two_num_sum.go
@@ -1,15 +1,39 @@
 package arrays
 
-import  (
+import (
 	"sort"
 )
 
-func TwoNumberSum(array []int, target int) []int {
+// TwoNumberSum_Iterative ...
+//
+// T -> O(n ** 2)
+// S -> O(1)
+func TwoNumberSum_Iterative(array []int, target int) []int {
+	for _, n := range array {
+		for _, nPrime := range array {
+			if n == nPrime {
+				continue
+			}
+
+			if n+nPrime == target {
+				return []int{n, nPrime}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+// TwoNumberSum_Sorted ...
+//
+// T -> O(nlog(n))
+// S -> O(1)
+func TwoNumberSum_Sorted(array []int, target int) []int {
 	sort.Ints(array)
-	l, r := 0, len(array) - 1
+	l, r := 0, len(array)-1
 	for l < r {
 		currentSum := array[l] + array[r]
-		if currentSum == target{
+		if currentSum == target {
 			return []int{array[l], array[r]}
 		} else if currentSum < target {
 			l++
@@ -18,4 +42,26 @@ func TwoNumberSum(array []int, target int) []int {
 		}
 	}
 	return make([]int, 0)
+}
+
+// TwoNumberSum_HashmapOnePass ...
+//
+// T -> O(n) where n is the number of numbers in numbers.
+// S -> O(n)
+func TwoNumberSum_HashmapOnePass(numbers []int, target int) []int {
+	// Since we want to find a + b = target.
+	// We can precompute (a - target) & store in a hashmap: {a-target: index of a}
+	//
+	// We can then in the same loop check if b exists - doing it in the same loop
+	// means we don't have to check to i==j case (e.g sum against ourself)
+	var hm = make(map[int]int)
+	for i, number := range numbers {
+		if j, ok := hm[number]; ok {
+			return []int{i, j}
+		}
+
+		hm[number] = i
+	}
+
+	return []int{}
 }
--- arrays/two_num_sum_test.go
+++ arrays/two_num_sum_test.go
@@ -4,16 +4,24 @@ import (
 	"testing"
 )
 
+var twoSumCheckers = []func([]int, int) []int{
+	TwoNumberSum_HashmapOnePass,
+	TwoNumberSum_Sorted,
+}
+
 func TestTwoNumberSum(t *testing.T) {
 	arr := []int{0, 1, 2, 3, 4, 5}
 	target := 8
 	expected := []int{3, 5}
 
-	res := TwoNumberSum(arr, target)
+	for _, c := range twoSumCheckers {
+		res := c(arr, target)
 
-	if !equals(res, expected) {
-		t.Fatal()
+		if !equals(res, expected) {
+			t.Fatal()
+		}
 	}
+
 }
 
 func equals(a, b []int) bool {
