200c6c6acb9ae27051b0a5e48ec23b1076768122
alexjperkins <alexjperkins@hotmail.co.uk>
2022-03-01 21:20:16 +0000

    add(linkedlist): reverse linked list iterative

--- linkedlists/reverse.go
+++ linkedlists/reverse.go
@@ -22,3 +22,22 @@ func reverse(curr, prev *LinkedList) *LinkedList {
 	curr.Next = prev
 	return reverse(next, curr)
 }
+
+// ReverseIterative ...
+//
+// T -> O(n)
+// S -> O(1)
+func ReverseIterative(head *LinkedList) *LinkedList {
+	var (
+		previous *LinkedList
+		current  = head
+	)
+	for current != nil {
+		next := current.Next
+		current.Next = previous
+		previous = current
+		current = next
+	}
+
+	return previous
+}
