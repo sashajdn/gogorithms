78a1ae2fef5c751c29e32eaf5d8075e2370872d7
alexjperkins <alexjperkins@hotmail.co.uk>
2022-03-07 21:21:55 +0000

    add(recursion): add powerset recursive

--- recursion/powerset.go
+++ recursion/powerset.go
@@ -2,7 +2,7 @@ package recursion
 
 // Powerset ...
 //
-// T -> O(n * n**2)
+// T -> O(n * 2 ** n)
 // S -> O(n * n**2)
 func Powerset(array []int) [][]int {
 	powerset := [][]int{{}}
@@ -17,4 +17,28 @@ func Powerset(array []int) [][]int {
 	}
 
 	return powerset
+}
+
+// Powerset_Recursive ...
+// T -> O(n * 2 ** n)
+// S -> O(k)
+func Powerset_Recursive(array []int) [][]int {
+	var output = [][]int{{}}
+	powerset(array, &output)
+	return output
+}
+
+func powerset(array []int, collector *[][]int) {
+	if len(array) == 0 {
+		return
+	}
+
+	h, t := array[0], array[1:]
+	l := len(*collector)
+
+	for i := 0; i < l; i++ {
+		*collector = append(*collector, append((*collector)[i], h))
+	}
+
+	powerset(t, collector)
 }
