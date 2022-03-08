1f0b539fbd2d15bec5a3296cf239a6900163794f
alexjperkins <alexjperkins@hotmail.co.uk>
2022-03-08 21:22:22 +0000

    chore(graphs): shortest path in binary matrix

--- graphs/shortest_path_binary_matrix.go
+++ graphs/shortest_path_binary_matrix.go
@@ -1,67 +1,85 @@
 package graphs
 
+type Point struct {
+	x, y int
+}
+
 // ShortestPathBinaryMatrix ...
 //
 // T -> O(n) where n is the w*h of the grid.
 // S -> O(n)
 func ShortestPathBinaryMatrix(grid [][]int) int {
 	var seen = make([][]bool, 0, len(grid))
-	for i := 0; i < len(seen); i++ {
-		seen = append(seen, make([]bool, len(seen[0])))
+	for i := 0; i < len(grid); i++ {
+		seen = append(seen, make([]bool, len(grid[0])))
 	}
 
 	var (
-		count   int
+		count   = 1
 		minPath = -1
+		queue   = []*Point{{0, 0}}
 	)
-	search(grid, seen, 0, 0, count, &minPath)
+	search(grid, seen, count, &minPath, &queue)
 	return minPath
 }
 
-func search(grid [][]int, seen [][]bool, i, j, count int, minSumSoFar *int) {
-	if j == len(grid)-1 && i == len(grid[0])-1 {
-		count++
-		*minSumSoFar = min(*minSumSoFar, count)
-	}
-
-	if seen[j][i] {
-		return
-	}
-	seen[j][i] = true
-
-	if grid[j][i] == 1 {
-		return
-	}
-	if count >= *minSumSoFar {
+func search(grid [][]int, seen [][]bool, count int, minSumSoFar *int, queue *[]*Point) {
+	if len(*queue) == 0 {
 		return
 	}
 
-	for _, neighbour := range getNeighbours(grid, i, j) {
-		x, y := neighbour[0], neighbour[1]
-		search(grid, seen, x, y, count+1, minSumSoFar)
+	l := len(*queue)
+	for i := 0; i < l; i++ {
+		nextPair := (*queue)[0]
+		*queue = (*queue)[1:]
+
+		if nextPair.y == len(grid)-1 && nextPair.x == len(grid[0])-1 && grid[nextPair.y][nextPair.x] == 0 {
+			switch *minSumSoFar {
+			case -1:
+				*minSumSoFar = count
+			default:
+				*minSumSoFar = min(*minSumSoFar, count)
+			}
+		}
+
+		if seen[nextPair.y][nextPair.x] {
+			continue
+		}
+		seen[nextPair.y][nextPair.x] = true
+
+		if grid[nextPair.y][nextPair.x] == 1 {
+			continue
+		}
+
+		for _, neighbour := range getNeighbours(grid, nextPair.x, nextPair.y, seen) {
+			x, y := neighbour[0], neighbour[1]
+			if seen[y][x] {
+				continue
+			}
+
+			*queue = append(*queue, &Point{x, y})
+		}
 	}
+
+	search(grid, seen, count+1, minSumSoFar, queue)
 }
 
-func getNeighbours(grid [][]int, i, j int) [][]int {
+func getNeighbours(grid [][]int, i, j int, seen [][]bool) [][]int {
 	var nn = [][]int{
-		{i - 1, j - 1},
-		{i - 1, j},
-		{i - 1, j + 1},
-		{i, j - 1},
-		{i, j + 1},
-		{i + 1, j - 1},
-		{i + 1, j},
-		{i + 1, j + 1},
+		{i - 1, j - 1}, {i, j}, {i + 1, j - 1},
+		{i - 1, j}, {i + 1, j},
+		{i - 1, j + 1}, {i, j + 1}, {i + 1, j + 1},
 	}
 
 	var validNeighbours [][]int
 	for _, n := range nn {
 		x, y := n[0], n[1]
 
-		if x > 0 && x < len(grid[0])-1 && y > 0 && y < len(grid)-1 {
+		if x >= 0 && x < len(grid[0]) && y >= 0 && y < len(grid) && !seen[y][x] {
 			validNeighbours = append(validNeighbours, n)
 		}
 	}
+
 	return validNeighbours
 }
 
--- graphs/shortest_path_binary_matrix_test.go
+++ graphs/shortest_path_binary_matrix_test.go
@@ -14,7 +14,44 @@ func TestShortestPathBinaryMatrix(t *testing.T) {
 		input          [][]int
 		expectedOutput int
 	}{
-		{},
+		{
+			name: "example_one_successful",
+			input: [][]int{
+				{0, 0, 0},
+				{1, 1, 0},
+				{1, 1, 0},
+			},
+			expectedOutput: 4,
+		},
+		{
+			name: "example_two_failure",
+			input: [][]int{
+				{1, 0, 0},
+				{1, 1, 0},
+				{1, 1, 0},
+			},
+			expectedOutput: -1,
+		},
+		{
+			name: "example_four_failure",
+			input: [][]int{
+				{0, 0, 0},
+				{1, 1, 0},
+				{1, 1, 1},
+			},
+			expectedOutput: -1,
+		},
+		{
+			name: "example_three_multiple_paths",
+			input: [][]int{
+				{0, 0, 0, 0, 0},
+				{1, 0, 1, 1, 0},
+				{1, 0, 0, 1, 0},
+				{1, 0, 1, 0, 0},
+				{1, 1, 0, 0, 0},
+			},
+			expectedOutput: 5,
+		},
 	}
 
 	for _, tt := range tests {
