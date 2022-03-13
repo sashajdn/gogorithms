ebf753a66843470830667e5880cf730e00bdc569
alexjperkins <alexjperkins@hotmail.co.uk>
2022-03-13 12:49:31 +0000

    add(binarytree): lowest common ancestor

--- binary-tree/lowest_common_ancestor_of_binary_tree.go
+++ binary-tree/lowest_common_ancestor_of_binary_tree.go
@@ -25,3 +25,48 @@ func LowestCommonAncestorOfBinaryTree(root, a, b *BinaryTree) *BinaryTree {
 
 	return left
 }
+
+// LowestCommonAncestorOfBinaryTreeParent ...
+//
+// T -> O(n)
+// S -> O(n)
+func LowestCommonAncestorOfBinaryTreeParent_Recursive(p, q *BinaryTreeParent) *BinaryTreeParent {
+	return search(p, q, p, q)
+}
+
+func LowestCommonAncestorOfBinaryTreeParent_Iterative(p, q *BinaryTreeParent) *BinaryTreeParent {
+	p1, p2 := p, q
+	for p1 != p2 {
+		switch p1.Parent {
+		case nil:
+			p1 = q
+		default:
+			p1 = p1.Parent
+		}
+
+		switch p2.Parent {
+		case nil:
+			p2 = p
+		default:
+			p2 = p2.Parent
+		}
+	}
+
+	return p1
+}
+
+func search(p, q, p1, p2 *BinaryTreeParent) *BinaryTreeParent {
+	if p1 == p2 {
+		return p1
+	}
+
+	if p1.Parent != nil && p2.Parent != nil {
+		return search(p, q, p1.Parent, p2.Parent)
+	}
+
+	if p1.Parent == nil {
+		return search(p, q, q, p2.Parent)
+	}
+
+	return search(p, q, p1.Parent, q)
+}
