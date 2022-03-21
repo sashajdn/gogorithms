4838e61f69966b8107f2bbf20196e11097380fa8
alexjperkins <alexjperkins@hotmail.co.uk>
2022-03-21 20:21:56 +0000

    add(arrays): max sub array recursive

--- arrays/max_subarray.go
+++ arrays/max_subarray.go
@@ -26,3 +26,37 @@ func MaxSubArray(nums []int) int {
 
 	return maxSubArraySum
 }
+
+// MaxSubArray_Recursive ...
+//
+// T -> O(nlog(n)) where `n` is the number of elements in the input array.
+// S -> O(log(n)) since we in the worst case will have log(n) calls in the stack as we are recursing.
+func MaxSubArray_Recursive(nums []int) int {
+	return maxSumRecurse(nums, 0, len(nums)-1)
+}
+
+func maxSumRecurse(nums []int, l, r int) int {
+	if l > r {
+		return math.MinInt
+	}
+
+	var (
+		mid                                = (l + r) / 2
+		current, bestLeftSum, bestRightSum int
+	)
+
+	for i := mid - 1; i >= l; i-- {
+		current += nums[i]
+		bestLeftSum = max(bestLeftSum, current)
+	}
+
+	current = 0
+	for j := mid + 1; j <= r; j++ {
+		current += nums[j]
+		bestRightSum = max(bestRightSum, current)
+	}
+
+	combination := nums[mid] + bestLeftSum + bestRightSum
+
+	return max(combination, max(maxSumRecurse(nums, l, mid-1), maxSumRecurse(nums, mid+1, r)))
+}
