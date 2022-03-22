0b89b0184cac086a8b53aece527f2cb79ebc0913
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-03-22 09:29:26 +0000

    add(linkedlist): reverse iter (#133)

--- linkedlists/reverse.go
+++ linkedlists/reverse.go
@@ -3,7 +3,7 @@ package linkedlists
 // Reverse ...
 //
 // T -> O(n) where n is the length of the linked list.
-// S -> O(1)
+// S -> O(n)
 func Reverse(head *LinkedList) *LinkedList {
 	if head == nil {
 		return nil
@@ -21,4 +21,20 @@ func reverse(curr, prev *LinkedList) *LinkedList {
 	next := curr.Next
 	curr.Next = prev
 	return reverse(next, curr)
+}
+
+// ReverseIterative ...
+// T -> O(n) where `n` is the length of the linked list.
+// S -> O(1)
+func ReverseIterative(head *LinkedList) *LinkedList {
+	var (
+		previous *LinkedList
+		current  = head
+	)
+	for current != nil {
+		next := current.Next
+		current.Next = previous
+		previous, current = current, next
+	}
+	return previous
 }
