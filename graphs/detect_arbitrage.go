ec6aa07bb333d3723ce9ebc6be63f6c0a7d74dba
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-03-23 17:55:38 +0000

    add(graphs): detect arbitrage (#139)

---
+++ graphs/detect_arbitrage.go
@@ -0,0 +1,71 @@
+package graphs
+
+import "math"
+
+//  ArbitrageEdge ...
+type ArbitrageEdge struct {
+	To, From int
+	Cost     float64
+}
+
+// DetectArbitrage ...
+//
+// T -> O(v * v * v) where `v` is the number of exchanges.
+// S -> O(v * v)
+func DetectArbitrage(exchangeRates [][]float64) bool {
+	var edges []*ArbitrageEdge
+	for j := 0; j < len(exchangeRates); j++ {
+		for i := 0; i < len(exchangeRates[0]); i++ {
+			if i == j {
+				continue
+			}
+
+			edges = append(edges, &ArbitrageEdge{
+				From: j,
+				To:   i,
+				Cost: negativeLog(exchangeRates[j][i]),
+			})
+		}
+	}
+
+	numberOfExchanges := len(exchangeRates)
+
+	distances := make([]float64, numberOfExchanges)
+	for i := 0; i < len(distances); i++ {
+		distances[i] = math.MaxInt
+	}
+	distances[0] = 0 // We can start at any currency since we have a strongly connected graph.
+
+	for i := 0; i < numberOfExchanges; i++ {
+		for _, edge := range edges {
+			if distances[edge.From] == math.MaxInt {
+				distances[edge.To] = edge.Cost
+				continue
+			}
+
+			distances[edge.To] = floatMin(distances[edge.To], distances[edge.From]+edge.Cost)
+
+		}
+	}
+
+	for i := 0; i < numberOfExchanges; i++ {
+		for _, edge := range edges {
+			if distances[edge.From]+edge.Cost < distances[edge.To] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func negativeLog(value float64) float64 {
+	return -1 * math.Log(value)
+}
+
+func floatMin(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
