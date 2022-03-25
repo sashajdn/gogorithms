7558169533d89fb125210781dac1915618d95fc6
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-03-25 10:29:03 +0000

    chore(stacks): refactor basic calc (#144)

--- stacks/basic_calculator.go
+++ stacks/basic_calculator.go
@@ -1,32 +1,6 @@
 package stacks
 
-import (
-	"strconv"
-)
-
-type Operand rune
-
-const (
-	add Operand = '+'
-	sub         = '-'
-	mul         = '*'
-	div         = '/'
-)
-
-func (o Operand) Reduce(a, b int) int {
-	switch o {
-	case '+':
-		return a + b
-	case '-':
-		return a + b
-	case '*':
-		return a * b
-	case '/':
-		return a / b
-	default:
-		return 0
-	}
-}
+import "strconv"
 
 // BasicCalculator ...
 //
@@ -34,54 +8,60 @@ func (o Operand) Reduce(a, b int) int {
 // S -> O(1) we could have used a stack here for `O(n)`, but since this will have at most 1/2 items on it we can store as a var.
 func BasicCalculator(s string) int {
 	var (
-		sumSoFar, previousNumber, currentNumber int
-		operand                                 Operand = '+'
+		currentNumber int
+		currentValue  int
+		total         int
+		operator      rune
 	)
+
 	for _, r := range s {
 		switch {
 		case isDigit(r):
-			c, _ := strconv.Atoi(string(r))
-			currentNumber = setSign(operand, abs(currentNumber*10)+c)
-		case r == '+', r == '-':
-			previousNumber = operand.Reduce(previousNumber, currentNumber)
-			sumSoFar += previousNumber
-
-			currentNumber, previousNumber = 0, 0
-			operand = Operand(r)
-
-		case r == '*', r == '/':
-			previousNumber = operand.Reduce(previousNumber, currentNumber)
-			currentNumber = 0
-
-			operand = Operand(r)
+			currentNumber = 10*currentNumber + runeToInt(r)
+			continue
+		case r == ' ':
+			continue
 		}
 
+		switch r {
+		case '+', '-':
+			total += calculate(operator, currentValue, currentNumber)
+			currentNumber, currentValue = 0, 0
+			operator = r
+		case '*', '/':
+			currentValue = calculate(operator, currentValue, currentNumber)
+			currentNumber = 0
+			operator = r
+		}
 	}
 
-	sumSoFar += operand.Reduce(previousNumber, currentNumber)
+	total += calculate(operator, currentValue, currentNumber)
+	return total
+}
 
-	return sumSoFar
+func runeToInt(r rune) int {
+	i, _ := strconv.Atoi(string(r))
+	return i
 }
 
 func isDigit(r rune) bool {
-	if r > '9' || r < '0' {
+	if r < '0' || r > '9' {
 		return false
 	}
-
 	return true
 }
 
-func setSign(op Operand, num int) int {
-	if op == sub && num > 0 {
-		return -num
+func calculate(op rune, a, b int) int {
+	switch op {
+	case '+':
+		return a + b
+	case '-':
+		return a - b
+	case '/':
+		return a / b
+	case '*':
+		return a * b
 	}
-	return num
-}
 
-func abs(a int) int {
-	if a < 0 {
-		return -a
-	}
-
-	return a
+	return 0
 }
