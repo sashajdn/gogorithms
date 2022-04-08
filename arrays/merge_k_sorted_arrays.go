e07ea691c0ad18a9bef6211bc3201e9d5b730c51
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-04-08 21:10:56 +0100

    Feat/merge k sorted arrays (#179)
    * add(arrays): k sorted arrays
    
    * chore(arrays): k sorted arrays improve

---
+++ arrays/merge_k_sorted_arrays.go
@@ -0,0 +1,57 @@
+package arrays
+
+// MergeKSortedArrays ...
+//
+// T -> O(nlog(n))
+// S -> O(n)
+func MergeKSortedArrays(arrays [][]int) []int {
+	switch len(arrays) {
+	case 0:
+		return []int{}
+	case 1:
+		return arrays[0]
+	}
+
+	var end = len(arrays) - 1
+	for end > 0 {
+		var left, right = 0, end
+		for left < right {
+			arrays[left] = mergeTwoArrays(arrays[left], arrays[right])
+			left++
+			right--
+		}
+
+		end /= 2
+	}
+
+	return arrays[0]
+}
+
+func mergeTwoArrays(a, b []int) []int {
+	var (
+		output      = make([]int, 0, len(a)+len(b))
+		left, right = 0, 0
+	)
+
+	for left < len(a) && right < len(b) {
+		if a[left] < b[right] {
+			output = append(output, a[left])
+			left++
+			continue
+		}
+
+		output = append(output, b[right])
+		right++
+	}
+
+	for left < len(a) {
+		output = append(output, a[left])
+		left++
+	}
+	for right < len(b) {
+		output = append(output, b[right])
+		right++
+	}
+
+	return output
+}
