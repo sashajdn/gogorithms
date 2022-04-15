54081749bce1f9da3429bdb5d6151ebf362c76d4
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-04-15 18:05:25 +0100

    add(arrays): iterative find peak (#194)

--- arrays/find_peak.go
+++ arrays/find_peak.go
@@ -1,6 +1,34 @@
 package arrays
 
-func findPeakElement(nums []int) int {
+// FindPeakElement_Iterative ...
+//
+// T -> O(log(n))
+// S -> O(1)
+func FindPeakElement_Iterative(nums []int) int {
+	var left, right = 0, len(nums) - 1
+	for left < right {
+		mid := (left + right) / 2
+
+		if mid+1 > len(nums)-1 {
+			return len(nums) - 1
+		}
+
+		if nums[mid+1] >= nums[mid] {
+			left = mid + 1
+			continue
+		}
+
+		right = mid
+	}
+
+	return left
+}
+
+// FindPeakElement_Recursive ...
+//
+// T -> O(log(n))
+// S -> O(log(n))
+func FindPeakElement_Recursive(nums []int) int {
 	return searchPeakElement(nums, 0, len(nums)+1)
 }
 
