9d943dd1c2b3ab2414583b3fddf2152d4060cb69
alexjperkins <alexjperkins@hotmail.co.uk>
2022-04-16 14:36:33 +0000

    add(bst): kth smallest element bst

---
+++ bst/kth_smallest_value.go
@@ -0,0 +1,40 @@
+package bst
+
+func KthSmallest(root *BinaryTree, k int) int {
+	var (
+		count   = 1
+		current = root
+	)
+	for current != nil {
+		if current.Left == nil {
+			if count == k || current.Right == nil {
+				return current.Value
+			}
+
+			count++
+			current = current.Right
+			continue
+		}
+
+		var rightmost = current.Left
+		for rightmost.Right != nil && rightmost.Right != current {
+			rightmost = rightmost.Right
+		}
+
+		if rightmost.Right == nil {
+			rightmost.Right = current
+			current = current.Left
+			continue
+		}
+
+		if count == k {
+			return current.Value
+		}
+
+		count++
+		rightmost.Right = nil
+		current = current.Right
+	}
+
+	return -1
+}
