e7a67689764673a5ec56f3087f27f7f66cf6e8f9
alexjperkins <alexjperkins@hotmail.co.uk>
2022-04-18 20:31:29 +0000

    add(graphs): dijkstras

--- graphs/dijkstras_pq.go
+++ graphs/dijkstras_pq.go
@@ -5,94 +5,38 @@ import (
 	"math"
 )
 
-type PQItem struct {
-	Value  int
-	Vertex int
-}
-
-type DijkstrasPQ struct {
-	h       []*PQItem
-	indexes map[int]int
-}
-
-func (d DijkstrasPQ) Len() int           { return len(d.h) }
-func (d DijkstrasPQ) Less(i, j int) bool { return d.h[i].Value < d.h[j].Value }
-func (d DijkstrasPQ) Swap(i, j int) {
-	d.h[i], d.h[j] = d.h[j], d.h[i]
-
-	d.indexes[i] = d.indexes[j]
-	d.indexes[j] = d.indexes[i]
-}
-func (d *DijkstrasPQ) Pop() interface{} {
-	v := d.h[d.Len()-1]
-	d.h = d.h[:d.Len()-1]
-	return v
-}
-func (d *DijkstrasPQ) Push(v interface{}) {
-	vv := v.(*PQItem)
-	d.h = append(d.h, vv)
-}
-
-func (d *DijkstrasPQ) UpdateValueAtIndex(index, value int) {
-	internalIndex := d.indexes[index]
-	d.h[internalIndex].Value = value
-	heap.Init(d)
-}
-
-// DijkstrasPq ...
-//
-// T -> O((v + e) * log(v))
-// S -> O(v)
-func DijkstrasPq(source int, edges [][][]int) []int {
+// DijkstrasPQ ...
+// T -> O((V + E) * log(V)) where `V` is the number of vertices & `E` the number of edges in G(V, E).
+// S -> O(V)
+func DijkstrasPQ(source int, edges [][][]int) []int {
 	var (
 		distances = make([]int, len(edges))
-		h         = make([]*PQItem, len(edges))
-		indexes   = make(map[int]int)
+		pq        = NewPQ(source, len(edges))
 	)
-
-	// T -> O(e)
-	// S -> O(v)
 	for i := 0; i < len(edges); i++ {
 		distances[i] = math.MaxInt
-		h[i] = &PQItem{
-			Value:  math.MaxInt,
-			Vertex: i,
-		}
-		indexes[i] = i
 	}
-
 	distances[source] = 0
-	h[source].Value = 0
 
-	// T -> O(vlog(v))
-	pq := &DijkstrasPQ{
-		h:       h,
-		indexes: indexes,
-	}
-	heap.Init(pq)
-
-	// T -> (e log(v))
-	// S -> O(v)
-	var visited = map[int]struct{}{}
-	for len(visited) < len(edges) {
+	// T -> O((V + E) * log(V))
+	// Since there is a total of E edges in G(V, E), we have (V + E) here rather than (VE)
+	for pq.Len() != 0 {
 		next := heap.Pop(pq)
-		from := next.(*PQItem)
+		item := next.(*PQItem)
+		from := item.Index
 
-		if from.Value == math.MaxInt {
+		if item.Value == math.MaxInt {
 			break
 		}
 
-		if _, ok := visited[from.Vertex]; ok {
-			continue
-		}
-		visited[from.Vertex] = struct{}{}
+		// T -> O(log(V) * e)  where `e` if the number of outbound edges at vertex, V.
+		for _, edge := range edges[from] {
+			to, distance := edge[0], edge[1]
 
-		for _, edge := range edges[from.Vertex] {
-			to, cost := edge[0], edge[1]
-
-			if distances[from.Vertex]+cost < distances[to] {
-				distances[to] = distances[from.Vertex] + cost
-				pq.UpdateValueAtIndex(to, distances[to])
+			pathDistance := distances[from] + distance
+			if pathDistance < distances[to] {
+				distances[to] = pathDistance
+				pq.UpdateValueAtIndex(pathDistance, to)
 			}
 		}
 	}
@@ -104,4 +48,57 @@ func DijkstrasPq(source int, edges [][][]int) []int {
 	}
 
 	return distances
+}
+
+type PQItem struct {
+	Value, Index int
+}
+
+func NewPQ(source, size int) *PQ {
+	var (
+		items      = make([]*PQItem, 0, size)
+		references = make(map[int]int, size)
+	)
+	for i := 0; i < size; i++ {
+		items = append(items, &PQItem{
+			Value: math.MaxInt,
+			Index: i,
+		})
+		references[i] = i
+	}
+	items[source].Value = 0
+
+	pq := &PQ{
+		items:      items,
+		references: references,
+	}
+	heap.Init(pq)
+
+	return pq
+}
+
+type PQ struct {
+	items      []*PQItem
+	references map[int]int
+}
+
+func (p PQ) Len() int           { return len(p.items) }
+func (p PQ) Less(i, j int) bool { return p.items[i].Value < p.items[j].Value }
+func (p PQ) Swap(i, j int) {
+	p.references[p.items[j].Index] = i
+	p.references[p.items[i].Index] = j
+	p.items[i], p.items[j] = p.items[j], p.items[i]
+}
+func (p *PQ) Pop() interface{} {
+	v := p.items[p.Len()-1]
+	p.items = p.items[:p.Len()-1]
+	return v
+}
+func (p *PQ) Push(value interface{}) {
+	vv := value.(*PQItem)
+	p.items = append(p.items, vv)
+}
+func (p *PQ) UpdateValueAtIndex(value, index int) {
+	p.items[p.references[index]].Value = value
+	heap.Init(p)
 }
