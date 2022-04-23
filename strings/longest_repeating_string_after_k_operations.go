07014f88067f792ef0aaf4aca2e5c9ad4b140957
alexjperkins <alexjperkins@hotmail.co.uk>
2022-04-23 21:05:24 +0000

    add(strings): longest repeating string after k operations

---
+++ strings/longest_repeating_string_after_k_operations.go
@@ -0,0 +1,41 @@
+package strings
+
+// LongestRepeatingStringAfterKOperations ...
+//
+// T -> O(n)
+// S -> O(charset) -> O(1)
+func LongestRepeatingStringAfterKOperations(s string, k int) int {
+	var (
+		frequencies                      = make([]int, 26)
+		left, mostFrequentChar, maxSoFar int
+	)
+	for right := 0; right < len(s); right++ {
+		rightIndex := runeToIndex(rune(s[right]))
+		frequencies[rightIndex]++
+
+		mostFrequentChar = max(mostFrequentChar, frequencies[rightIndex])
+
+		if (right-left+1)-mostFrequentChar > k {
+			leftIndex := runeToIndex(rune(s[left]))
+			frequencies[leftIndex]--
+			left++
+			continue
+		}
+
+		maxSoFar = max(maxSoFar, right-left+1)
+	}
+
+	return maxSoFar
+}
+
+func runeToIndex(r rune) int {
+	return int(r - 'A')
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
+}
