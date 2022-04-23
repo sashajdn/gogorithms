00ac5c4d3d959cb69374583509b47f44d7b23e7a
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-04-23 19:47:13 +0100

    add(strings): group anagrams (#220)

---
+++ strings/group_anagrams.go
@@ -0,0 +1,43 @@
+package strings
+
+import (
+	"fmt"
+	"strings"
+)
+
+// GroupAnagrams ...
+//
+// T -> O(n * w)
+// S -> O(n * w)
+func GroupAnagrams(words []string) [][]string {
+	var hm = make(map[string][]string, len(words))
+	for _, word := range words {
+		var counter = make(Counter, len(word))
+		for _, r := range word {
+			counter[r]++
+		}
+
+		key := counter.Hash()
+		hm[key] = append(hm[key], word)
+	}
+
+	var output = make([][]string, 0, 0)
+	for _, group := range hm {
+		output = append(output, group)
+	}
+
+	return output
+}
+
+type Counter map[rune]int
+
+func (c Counter) Hash() string {
+	var sb strings.Builder
+	for r := 'a'; r <= 'z'; r++ {
+		if count, ok := c[r]; ok {
+			sb.WriteString(fmt.Sprintf("#%s%d", r, count))
+		}
+	}
+
+	return sb.String()
+}
