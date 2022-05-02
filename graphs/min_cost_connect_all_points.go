e2083cf26f1ab12dc1affd8a319b8fce1999bc75
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-05-02 09:37:46 +0100

    add(graphs): min cost connect all paths prims lazy (#239)

--- graphs/min_cost_connect_all_points.go
+++ graphs/min_cost_connect_all_points.go
@@ -1,12 +1,78 @@
 package graphs
 
-import "sort"
+import (
+	"container/heap"
+	"sort"
+)
 
-// MinCostConnectAllPoints ...
+// MinCostConnectAllPoints_PrimsLazy ...
+//
+// T -> O(Elog(E)) ~> O((n ** 2) * log(n))
+// S -> O(E) -> O(n ** 2)
+func MinCostConnectAllPoints_PrimsLazy(points [][]int) int {
+	var (
+		visited = make([]bool, len(points))
+		pq      = &PointEdgePQ{}
+	)
+	heap.Push(pq, &PointEdge{
+		From: 0,
+		To:   0,
+		Cost: 0,
+	})
+
+	// T -> O(Elog(E)) -> O((n**2)log(n ** 2)) ~> O((n ** 2) log(n))
+	// S -> O(E) -> O(n ** 2)
+	var edgeCount, mstCost int
+	for edgeCount < len(points) {
+		next := heap.Pop(pq)
+		edge := next.(*PointEdge)
+
+		if visited[edge.To] {
+			continue
+		}
+		visited[edge.To] = true
+
+		edgeCount++
+		mstCost += edge.Cost
+
+		for i := 0; i < len(points); i++ {
+			if visited[i] {
+				continue
+			}
+
+			p1, p2 := points[edge.To], points[i]
+			cost := intAbs(p1[0]-p2[2]) + intAbs(p1[1]-p2[1])
+
+			heap.Push(pq, &PointEdge{
+				To:   i,
+				Cost: cost,
+			})
+		}
+	}
+
+	return mstCost
+}
+
+type PointEdgePQ []*PointEdge
+
+func (p PointEdgePQ) Len() int           { return len(p) }
+func (p PointEdgePQ) Less(i, j int) bool { return p[i].Cost < p[j].Cost }
+func (p PointEdgePQ) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p *PointEdgePQ) Push(v interface{}) {
+	vv := v.(*PointEdge)
+	*p = append(*p, vv)
+}
+func (p *PointEdgePQ) Pop() interface{} {
+	v := (*p)[p.Len()-1]
+	*p = (*p)[:p.Len()-1]
+	return v
+}
+
+// MinCostConnectAllPoints_Kruskals ...
 //
 // T -> O(elog(e) + elog(v)) where e is the number of edges and is equal to (n ** 2) since we have a complete graph.
 // S -> O(n ** 2)
-func MinCostConnectAllPoints(points [][]int) int {
+func MinCostConnectAllPoints_Kruskals(points [][]int) int {
 	// T -> O(n ** 2)
 	// S -> O(n ** 2)
 	var edgeList []*PointEdge
