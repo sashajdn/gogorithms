149d3c1a4a6757b62d5846415a1016ee91259bd6
alexjperkins <alexjperkins@hotmail.co.uk>
2022-05-20 17:25:21 +0000

    add(dynamic): fibonacci

---
+++ dynamic/fibonacci_test.go
@@ -0,0 +1,54 @@
+package dynamic
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var fibonnaciChecks = []func(n int) int{
+	Fibonacci_BottomUp,
+	Fibonacci_TopDown,
+}
+
+func TestFibonacci(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name           string
+		n              int
+		expectedOutput int
+	}{
+		{
+			name:           "0",
+			n:              0,
+			expectedOutput: 0,
+		},
+		{
+			name:           "1",
+			n:              1,
+			expectedOutput: 1,
+		},
+		{
+			name:           "2",
+			n:              2,
+			expectedOutput: 1,
+		},
+		{
+			name:           "14",
+			n:              14,
+			expectedOutput: 377,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			for _, c := range fibonnaciChecks {
+				assert.Equal(t, tt.expectedOutput, c(tt.n))
+			}
+		})
+	}
+}
