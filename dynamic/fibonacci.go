6212d3414a023371bdcd8c6027be98f127a2b204
Alex Perkins <31310384+alexjperkins@users.noreply.github.com>
2022-05-20 18:25:54 +0100

    add(dynamic): fibonacci (#268)

---
+++ dynamic/fibonacci.go
@@ -0,0 +1,50 @@
+package dynamic
+
+// Fibonacci_BottomUp ...
+//
+// T -> O(n)
+// S -> O(n)
+func Fibonacci_BottomUp(n int) int {
+	var memo = map[int]int{}
+
+	var dfs func(n int) int
+	dfs = func(n int) int {
+		switch n {
+		case 0:
+			return 0
+		case 1, 2:
+			return 1
+		}
+
+		if v, ok := memo[n]; ok {
+			return v
+		}
+
+		memo[n] = dfs(n-1) + dfs(n-2)
+		return memo[n]
+	}
+
+	return dfs(n)
+}
+
+// Fibonacci_TopDown ...
+//
+// T -> O(n)
+// S -> O(n)
+func Fibonacci_TopDown(n int) int {
+	switch n {
+	case 0:
+		return 0
+	case 1, 2:
+		return 1
+	}
+
+	var dp = make([]int, n+1)
+	dp[1], dp[2] = 1, 1
+
+	for i := 2; i < n+1; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
